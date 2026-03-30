0bbb6ee515e2de806c71fd08f1c6274070f81480
Geordany <geordany1905@gmail.com>
2026-03-30 11:04:09 -0400

    Mis cambios de hoy

---
+++ main.py
@@ -0,0 +1,34 @@
+def menu_principal():
+    gestion = DoctorServicios()
+    
+    while True:
+        print("\n=== SISTEMA MÉDICO - MÓDULO GEORDANY ===")
+        print("1. Agregar Paciente")
+        print("2. Listar Pacientes")
+        print("3. Agregar Doctor")
+        print("4. Listar Doctores")
+        print("5. Salir")
+        
+        opcion = input("Seleccione una opción: ")
+        
+        if opcion == "1":
+            id_p = input("ID: ")
+            nom = input("Nombre: ")
+            edad = input("Edad: ")
+            gestion.crear_paciente(nom, edad, id_p)
+        elif opcion == "2":
+            gestion.listar_pacientes()
+        elif opcion == "3":
+            id_d = input("ID: ")
+            nom = input("Nombre: ")
+            esp = input("Especialidad: ")
+            gestion.crear_doctor(nom, esp, id_d)
+        elif opcion == "4":
+            gestion.listar_doctores()
+        elif opcion == "5":
+            break
+        else:
+            print("Opción no válida.")
+
+if __name__ == "__main__":
+    menu_principal()
